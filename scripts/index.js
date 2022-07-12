5507f6bafb375ebbe5883b4ed4eab120f3f833e0
Olya Ryzhkova <OJRyzhkova@yandex.ru>
2022-07-13 01:47:58 +0300

    java script

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,45 @@
 let popupElement = document.querySelector('.popup');
 let popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 let popupOpeneButtonElement = document.querySelector('.profile__open-popup');
-let FormElement = popupElement.querySelector('.popup__form');
+let formElement = popupElement.querySelector('.popup__form');
 let Name = document.querySelector('.profile__name');
 let Job = document.querySelector('.profile__description');
 let NameInput = popupElement.querySelector('.popup__input-name');
-let NameJob = popupElement.querySelector('.popup__input-job');
+let JobInput = popupElement.querySelector('.popup__input-job');
 
 NameInput.value = Name.textContent;
+JobInput.value = Job.textContent;
 
 
 
-
-
-
-//const togglePopupVisibility = function(){
-  //  popupElement.classList.toggle('popup_opened');
-
-//}
-
-//togglePopupVisibility();
-//popupOpeneButtonElement.addEventListener('click', togglePopupVisibility);
-
-let openPopup = function(event) {
+let openPopup = function (event) {
     popupElement.classList.add('popup_opened');
     console.log('open popup clicked');
 };
-let closePopup = function() {
+let closePopup = function () {
     popupElement.classList.remove('popup_opened');
 };
 
-let closePopupByClickOnOverlay = function(event) {
+let closePopupByClickOnOverlay = function (event) {
     console.log(event.target, event.currentTarget);
     if (event.target !== event.currentTarget) {
         return
     }
     closePopup();
 }
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    Name.textContent = NameInput.value;
+    Job.textContent = JobInput.value;
+    closePopup();
+}
+
+
+
 popupOpeneButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 popupElement.addEventListener('click', closePopupByClickOnOverlay);
+formElement.addEventListener('submit', formSubmitHandler);
 
 
 
